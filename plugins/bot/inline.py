4de138d0c7f604e3b1f0c39f37a7fa5892492061
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-08 01:15:16 +0600

    Create inline.py

---
+++ plugins/bot/inline.py
@@ -0,0 +1,105 @@
+"""
+RadioPlayerV3, Telegram Voice Chat Bot
+Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import asyncio
+from config import Config
+from utils import USERNAME
+from pyrogram import Client, errors
+from youtubesearchpython import VideosSearch
+from pyrogram.handlers import InlineQueryHandler
+from pyrogram.types import InlineQueryResultArticle, InlineQueryResultPhoto, InputTextMessageContent, InlineKeyboardButton, InlineKeyboardMarkup
+
+REPLY_MESSAGE=Config.REPLY_MESSAGE
+
+buttons = [
+            [
+                InlineKeyboardButton("❔ HOW TO USE ME ❔", callback_data="help"),
+            ],
+            [
+                InlineKeyboardButton("CHANNEL", url="https://t.me/XteamBD"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/XteamBDChat"),
+            ],
+            [
+                InlineKeyboardButton("🤖 DEV 🤖", url="https://t.me/BotMakerBD"),
+            ]
+         ]
+
+
+@Client.on_inline_query()
+async def search(client, query):
+    answers = []
+    if query.query == "SAF_ONE":
+        answers.append(
+            InlineQueryResultPhoto(
+                title="Deploy Your Own Radio Player",
+                thumb_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
+                photo_url="https://telegra.ph/file/4e839766d45935998e9c6.jpg",
+                caption=f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@BotMakerBD | @XteamBD 👑</b>",
+                reply_markup=InlineKeyboardMarkup(buttons)
+                )
+            )
+        await query.answer(results=answers, cache_time=0)
+        return
+    string = query.query.lower().strip().rstrip()
+    if string == "":
+        await client.answer_inline_query(
+            query.id,
+            results=answers,
+            switch_pm_text=("✍️ Type An Video Name !"),
+            switch_pm_parameter="help",
+            cache_time=0
+        )
+    else:
+        videosSearch = VideosSearch(string.lower(), limit=50)
+        for v in videosSearch.result()["result"]:
+            answers.append(
+                InlineQueryResultArticle(
+                    title=v["title"],
+                    description=("Duration: {} Views: {}").format(
+                        v["duration"],
+                        v["viewCount"]["short"]
+                    ),
+                    input_message_content=InputTextMessageContent(
+                        "/play https://www.youtube.com/watch?v={}".format(
+                            v["id"]
+                        )
+                    ),
+                    thumb_url=v["thumbnails"][0]["url"]
+                )
+            )
+        try:
+            await query.answer(
+                results=answers,
+                cache_time=0
+            )
+        except errors.QueryIdInvalid:
+            await query.answer(
+                results=answers,
+                cache_time=0,
+                switch_pm_text=("Error: Search Timed Out!"),
+                switch_pm_parameter="",
+            )
+
+
+__handlers__ = [
+    [
+        InlineQueryHandler(
+            search
+        )
+    ]
+]
