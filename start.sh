fb62447a81edc185aadc5d0c9e45c820c13f3471
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-08 02:40:39 +0600

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-echo "Cloning Repo, Please Wait..."
-git clone https://github.com/wajirr4/Vipmusic.git /Vipmusic
-echo "Installing Requirements..."
-cd /Vipmusic
-pip3 install -U -r requirements.txt
-echo "Starting Bot, Please Wait..."
-python3 main.py
