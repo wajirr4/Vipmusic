13fb6c7f266e2a9a3cbd75994151ea972daf4a0b
wajirr4 <109944533+wajirr4@users.noreply.github.com>
2022-08-08 02:40:23 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 echo "Cloning Repo, Please Wait..."
-git clone https://github.com/AsmSafone/RadioPlayerV3.git /RadioPlayerV3
+git clone https://github.com/wajirr4/Vipmusic.git /Vipmusic
 echo "Installing Requirements..."
-cd /RadioPlayerV3
+cd /Vipmusic
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
 python3 main.py
